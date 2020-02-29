c15dfff5a4a11a34f83a92a0144ba749933cce15
geusan <dnay2k@gmail.com>
2020-02-29 23:57:00 +0900

    Modify test mocks

--- test/features/Todo/createTodo.spec.js
+++ test/features/Todo/createTodo.spec.js
@@ -12,14 +12,14 @@ describe('testTodo', () => {
         tags: ['prography', 'nodejs'],
       },
       {
-        title: '프로그라피 사전과제하기',
-        description: '프로그라피 NodeJS 사전과제가 상당히 까다롭다고 한다. 한번 도전 해보자!',
-        tags: ['prography', 'nodejs'],
+        title: '면접 준비',
+        description: '깔끔한 복장, 웃기는 아재개그 일발 장전. 멋진 인상으로 임팩트를 주자!',
+        tags: ['prography', 'interview', 'ajae-gag'],
       },
       {
-        title: '프로그라피 사전과제하기',
-        description: '프로그라피 NodeJS 사전과제가 상당히 까다롭다고 한다. 한번 도전 해보자!',
-        tags: ['prography', 'nodejs'],
+        title: '프로그라피에서 시원하게 개발하기',
+        description: '내가 만든 서비스가 세상을 바꾼다.',
+        tags: ['prography', 'new-project'],
       },
     ];
     for (let i = 0; i < 3; i += 1) {
