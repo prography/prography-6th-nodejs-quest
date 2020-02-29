b6e6894917cec23e7203f5b99e780d75894a272c
geusan <dnay2k@gmail.com>
2020-02-29 23:51:39 +0900

    Add test for creating item 3 times

--- test/features/Comment/createComment.spec.js
+++ test/features/Comment/createComment.spec.js
@@ -5,14 +5,21 @@ const testClient = supertest(app);
 
 describe('testComment', () => {
   test('createComment', async () => {
-    const payload = {
-      contents: ' 프로그라피 사전과제를 언제까지 할 수 있을까?',
-    };
-    const res = await testClient
-      .post(`/todos/${process.env.TEST_TODO_ID}/comments`)
-      .send(payload);
-    expect(res.status).toBe(200);
-    expect(res.body).toMatchObject(payload);
-    process.env.TEST_COMMENT_ID = res.body.id;
+    const payloads = [
+      { contents: '프로그라피 사전과제를 언제까지 할 수 있을까?' },
+      { contents: '프로그라피 사전과제는 금방 하지 않을까?' },
+      { contents: '프로그라피 사전과제는 정말 쉽지!' },
+    ]
+    for (let i = 0; i < 3; i += 1) {
+      const payload = payloads[i];
+      const res = await testClient
+        .post(`/todos/${process.env.TEST_TODO_ID}/comments`)
+        .send(payload);
+      expect(res.status).toBe(200);
+      expect(res.body).toMatchObject(payload);
+      process.env[`TEST_COMMENT_ID_${i}`] = res.body.id;
+    }
+    // 첫번째 test comment id를 수정, 삭제 키로 사용
+    process.env.TEST_COMMENT_ID = process.env.TEST_COMMENT_ID_0;
   })
 })
--- test/features/Todo/createTodo.spec.js
+++ test/features/Todo/createTodo.spec.js
@@ -5,16 +5,33 @@ const testClient = supertest(app);
 
 describe('testTodo', () => {
   test('createTodo', async () => {
-    const payload = {
-      title: '프로그라피 사전과제하기',
-      description: '프로그라피 NodeJS 사전과제가 상당히 까다롭다고 한다. 한번 도전 해보자!',
-      tags: ['prography', 'nodejs'],
-    };
-    const res = await testClient
-      .post('/todos')
-      .send(payload);
-    expect(res.status).toBe(200);
-    expect(res.body).toMatchObject(payload);
-    process.env.TEST_TODO_ID = res.body.id;
+    const payloads = [
+      {
+        title: '프로그라피 사전과제하기',
+        description: '프로그라피 NodeJS 사전과제가 상당히 까다롭다고 한다. 한번 도전 해보자!',
+        tags: ['prography', 'nodejs'],
+      },
+      {
+        title: '프로그라피 사전과제하기',
+        description: '프로그라피 NodeJS 사전과제가 상당히 까다롭다고 한다. 한번 도전 해보자!',
+        tags: ['prography', 'nodejs'],
+      },
+      {
+        title: '프로그라피 사전과제하기',
+        description: '프로그라피 NodeJS 사전과제가 상당히 까다롭다고 한다. 한번 도전 해보자!',
+        tags: ['prography', 'nodejs'],
+      },
+    ];
+    for (let i = 0; i < 3; i += 1) {
+      const payload = payloads[i];
+      const res = await testClient
+        .post('/todos')
+        .send(payload);
+      expect(res.status).toBe(200);
+      expect(res.body).toMatchObject(payload);
+      process.env[`TEST_TODO_ID_${i}`] = res.body.id;
+    }
+    // 첫번째 test todo id를 수정, 삭제 키로 사용
+    process.env.TEST_TODO_ID = process.env.TEST_TODO_ID_0;
   })
 })
