8980430be07df37548f6a6aaf4e77fffb0ce08bb
ehddnr301 <dy950328@gmail.com>
2020-03-01 19:18:07 +0900

    initial config

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
-import express from 'express';
+import express from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
 
 const app = express();
 
-app.use('/hello-world', (req, res) => {
-  res.send('hello world');
-})
+if (process.env.NODE_ENV !== "test") {
+  app.use(morgan("dev"));
+}
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/hello-world", (req, res) => {
+  res.send("hello world");
+});
 
 export default app;
