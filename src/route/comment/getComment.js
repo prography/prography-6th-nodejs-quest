bc8dba1263f2f1111caf4e44856bbb08f485408f
KimGenius <geniusk1047@naver.com>
2020-03-01 18:43:12 +0900

    댓글 읽기 작업

---
+++ src/route/comment/getComment.js
@@ -0,0 +1,8 @@
+'use strict'
+import Comment from '../../models/comment'
+
+module.exports = async (req, res) => {
+  const {todoId, commentId} = req.params
+  const result = await Comment.find({todoId, id: commentId}).select('-_id -todoId')
+  return res.json(result)
+}
