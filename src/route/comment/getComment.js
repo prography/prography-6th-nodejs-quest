7ba55dd005f3837a2d483c4804e979baa0b01e02
KimGenius <geniusk1047@naver.com>
2020-03-01 18:49:00 +0900

    댓글 테스트 통과

--- src/route/comment/getComment.js
+++ src/route/comment/getComment.js
@@ -4,5 +4,5 @@ import Comment from '../../models/comment'
 module.exports = async (req, res) => {
   const {todoId, commentId} = req.params
   const result = await Comment.find({todoId, id: commentId}).select('-_id -todoId')
-  return res.json(result)
+  return res.json(result[0])
 }
